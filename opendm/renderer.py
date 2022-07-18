5b8f4a6004ea3b63cb2337bb89fab2ed4a0f192e
Piero Toffanin <pt@masseranolabs.com>
2022-07-18 16:44:25 -0400

    Rendering PoC

--- opendm/renderer.py
+++ opendm/renderer.py
@@ -4,6 +4,8 @@ import numpy as np
 import warnings
 import os
 
+# Originally ported from https://github.com/OpenDroneMap/odm_orthophoto
+
 warnings.filterwarnings("ignore", category=rasterio.errors.NotGeoreferencedWarning)
 
 def _quiet(msg):
@@ -11,6 +13,9 @@ def _quiet(msg):
 
 _info = _quiet
 _warn = _quiet
+
+eps = np.finfo(float).eps
+eps2 = eps * eps
 
 class Bbox:
     def __init__(self):
@@ -79,7 +84,7 @@ def load_obj(obj_path):
                 vertices.append(list(map(float, line.split()[1:4])))
             elif line.startswith("vt "):
                 # UVs
-                uvs.append([list(map(float, line.split()[1:3]))])
+                uvs.append(list(map(float, line.split()[1:3])))
             elif line.startswith("usemtl "):
                 mtl_name = "".join(line.split()[1:]).strip()
                 if not mtl_name in obj['materials']:
@@ -95,7 +100,7 @@ def load_obj(obj_path):
                 if current_material not in faces:
                     faces[current_material] = []
 
-                faces[current_material].append(((av, bv, cv), (at, bt, ct))) 
+                faces[current_material].append(((av - 1, bv - 1, cv - 1), (at - 1, bt - 1, ct - 1))) 
 
     obj['vertices'] = np.array(vertices)
     obj['uvs'] = np.array(uvs)
@@ -144,8 +149,29 @@ def max_range(dtype):
     except ValueError:
         return np.finfo(dtype).max
 
+def is_sliver_polygon(v1, v2, v3):
+    dummy_vec = np.cross(v1 - v2, v3 - v2)
+    return eps2 >= dummy_vec.dot(dummy_vec) / 2.0
 
-def render_orthophoto(input_objs, resolution=5.0):
+def get_barycentric_coordinates(v1, v2, v3, x, y):
+    # Diff along y
+    y2y3 = v2[1] - v3[1]
+    y1y3 = v1[1] - v3[1]
+    y3y1 = v3[1] - v1[1]
+    yy3 = y - v3[1]
+
+    # Diff along x
+    x3x2 = v3[0] - v2[0]
+    x1x3 = v1[0] - v3[0]
+    xx3 = x - v3[0]
+
+    norm = y2y3 * x1x3 + x3x2 * y1y3
+    l1 = (y2y3 * xx3 + x3x2 * yy3) / norm
+    l2 = (y3y1 * xx3 + x1x3 * yy3) / norm
+    l3 = 1 - l1 - l2
+    return (l1, l2, l3)
+
+def render_orthophoto(input_objs, resolution, output):
     """Render OBJ files
     :param input_objs (str[]) array of paths to .OBJ models
     :param resolution (float) resolution of resulting raster in cm/px
@@ -193,16 +219,14 @@ def render_orthophoto(input_objs, resolution=5.0):
 
         _info("Rendering the orthophoto...")
 
-        # Used to keep track of the global face index
-        faceOff = 0 # TODO: necessary?
-
         # Iterate over each part of the mesh (one per material)
         data_type = None
-        current_band_index = 0 # TODO: necessary?
+        current_band_index = 0
         texture = None
 
         bands = []
-        alpha_band = np.zeros((width, height))
+        alpha_band = None
+        depth = np.full((width, height), -np.inf, dtype=np.float32)
         materials_idx = list(obj['materials'].keys())
 
         for mat in materials_idx:
@@ -224,24 +248,288 @@ def render_orthophoto(input_objs, resolution=5.0):
 
                 fill_value = max_range(data_type)
                 for n in range(texture.shape[2]):
-                    band = np.full((width, height), fill_value)
+                    band = np.full((width, height), fill_value, dtype=data_type)
                     bands.append(band)
+                alpha_band = np.zeros((width, height), dtype=data_type)
+        
 
             # Render
 
+            rows, cols = map(float, texture.shape[:2])
+            num_channels = texture.shape[2]
+
             # For each face in the current material
             for f in obj['faces'][mat]:
-                print(f)
-        
-        if texture is not None:
-            current_band_index += texture.shape[2]
-        
+                # Draw textured triangle
+
+                v1i, v2i, v3i = f[0]
+                v1 = obj['vertices'][v1i]
+                v2 = obj['vertices'][v2i]
+                v3 = obj['vertices'][v3i]
+
+                if is_sliver_polygon(v1, v2, v3):
+                    _warn("Sliver polygon found at %s" % f)
+                    continue
+
+                u1i, u2i, u3i = f[1]
+                v1t = obj['uvs'][u1i]
+                v2t = obj['uvs'][u2i]
+                v3t = obj['uvs'][u3i]
+
+                # Check bounding box overlap
+                
+                minx = int(min((v1[0], v2[0], v3[0])))
+                if minx > width:
+                    continue # Completely outside to the right
+                maxx = int(max((v1[0], v2[0], v3[0])))
+                if maxx < 0:
+                    continue # Completely outside to the left
+                miny = int(min((v1[1], v2[1], v3[1])))
+                if miny > height:
+                    continue # Completely outside to the top.
+                maxy = int(max((v1[1], v2[1], v3[1])))
+                if maxy < 0:
+                    continue # Completely outside to the bottom.
+                
+                # Top point row and column positions
+                top_r = top_c = None
+                # Middle point row and column positions
+                mid_r = mid_c = None
+                # Bottom point row and column positions
+                bot_r = bot_c = None
+
+                if v1[1] < v2[1]:
+                    if v1[1] < v3[1]:
+                        if v2[1] < v3[1]:
+                            # 1 -> 2 -> 3
+                            top_r, top_c = v1[1], v1[0]
+                            mid_r, mid_c = v2[1], v2[0]
+                            bot_r, bot_c = v3[1], v3[0]
+                        else:
+                            # 1 -> 3 -> 2
+                            top_r, top_c = v1[1], v1[0]
+                            mid_r, mid_c = v3[1], v3[0]
+                            bot_r, bot_c = v2[1], v2[0]
+                    else:
+                        # 3 -> 1 -> 2
+                        top_r, top_c = v3[1], v3[0]
+                        mid_r, mid_c = v1[1], v1[0]
+                        bot_r, bot_c = v2[1], v2[0]
+                else: # v2[1] <= v1[1]
+                    if v2[1] < v3[1]:
+                        if v1[1] < v3[1]:
+                            # 2 -> 1 -> 3
+                            top_r, top_c = v2[1], v2[0]
+                            mid_r, mid_c = v1[1], v1[0]
+                            bot_r, bot_c = v3[1], v3[0]
+                        else:
+                            # 2 -> 3 -> 1
+                            top_r, top_c = v2[1], v2[0]
+                            mid_r, mid_c = v3[1], v3[0]
+                            bot_r, bot_c = v1[1], v1[0]
+                    else:
+                        # 3 -> 2 -> 1
+                        top_r, top_c = v3[1], v3[0]
+                        mid_r, mid_c = v2[1], v2[0]
+                        bot_r, bot_c = v1[1], v1[0]
+                # rows = float(height)
+                # cols = float(width)
+
+                # General appreviations:
+                # ---------------------
+                # tm : Top(to)Middle.
+                # mb : Middle(to)Bottom.
+                # tb : Top(to)Bottom.
+                # c  : column.
+                # r  : row.
+                # dr : DeltaRow, step value per row.
+
+                # The step along column for every step along r. Top to middle.
+                ctmdr = None
+                # The step along column for every step along r. Top to bottom.
+                ctbdr = None
+                # The step along column for every step along r. Middle to bottom.
+                cmbdr = None
+
+                ctbdr = (bot_c - top_c) / (bot_r - top_r)
+
+                # The current column position, from top to middle.
+                ctm = top_c
+                # The current column position, from top to bottom.
+                ctb = top_c
+
+                # Check for vertical line between middle and top.
+                if eps < mid_r - top_r:
+                    ctmdr = (mid_c - top_c) / (mid_r - top_r)
+
+                    # The first pixel row for the bottom part of the triangle.
+                    rq_start = max(int(np.floor(top_r + 0.5)), 0)
+                    
+                    # The last pixel row for the top part of the triangle.
+                    rq_end = min(int(np.floor(mid_r + 0.5)), height)
+
+                    for rq in range(rq_start, rq_end):
+                        # Set the current column positions.
+                        ctm = top_c + ctmdr * (float(rq)+0.5-top_r)
+                        ctb = top_c + ctbdr * (float(rq)+0.5-top_r)
+
+                        # The first pixel column for the current row
+                        cq_start = max(int(np.floor(0.5 + min(ctm, ctb))), 0)
+                        
+                        # The last pixel column for the current row.
+                        cq_end = min(int(np.floor(0.5 + max(ctm, ctb))), width)
+
+                        for cq in range(cq_start, cq_end):
+                            l1, l2, l3 = get_barycentric_coordinates(v1, v2, v3, float(cq) + 0.5, float(rq) + 0.5)
+                            if l1 < 0 or l2 < 0 or l3 < 0:
+                                continue
+
+                            z = v1[2] * l1 + v2[2] * l2 + v3[2] * l3
+
+                            depth_value = depth[cq][rq]
+                            if z < depth_value:
+                                # Current is behind another, don't draw
+                                continue
+
+                            # The uv values of the point
+                            u = v1t[0] * l1 + v2t[0] * l2 + v3t[0] * l3
+                            v = v1t[1] * l1 + v2t[1] * l2 + v3t[1] * l3
+
+                            # Render pixel
+                            s = u * float(cols)
+                            t = (1.0 - v) * float(rows)
+
+                            dl, left_f = np.modf(s)
+                            dr = 1.0 - dl
+                            dt, top_f = np.modf(t)
+                            db = 1.0 - dt
+
+                            left = int(left_f)
+                            top = int(top_f)
+
+                            for i in range(0, num_channels):
+                                value = 0.0
+
+                                tl = float(texture[top][left][i])
+                                tr = float(texture[top][left+1][i])
+                                bl = float(texture[top+1][left][i])
+                                br = float(texture[top+1][left+1][i])
+
+                                value += tl * dr * db
+                                value += tr * dl * db
+                                value += bl * dr * dt
+                                value += br * dl * dt
+
+                                bands[current_band_index + i][cq][rq] = value
+
+                            # Increment the alpha band if the pixel was visible for this band
+                            # the final alpha band will be set to 255 if alpha == num bands
+                            # (all bands have information at this pixel)
+                            alpha_band[cq][rq] += num_channels
+
+                            # Update depth buffer
+                            depth[cq][rq] = z
+                
+                if eps < bot_r - mid_r:
+                    cmbdr = (bot_c - mid_c) / (bot_r - mid_r)
+
+                    # The current column position, from middle to bottom.
+                    cmb = mid_c
+
+                    # The first pixel row for the bottom part of the triangle.
+                    rq_start = max(int(np.floor(mid_r + 0.5)), 0)
+                    
+                    # The last pixel row for the top part of the triangle.
+                    rq_end = min(int(np.floor(bot_r + 0.5)), height)
+
+                    for rq in range(rq_start, rq_end):
+                        # Set the current column positions.
+                        ctb = top_c + ctbdr * (float(rq)+0.5-top_r)
+                        cmb = mid_c + cmbdr * (float(rq)+0.5-mid_r)
+
+                        # The first pixel column for the current row
+                        cq_start = max(int(np.floor(0.5 + min(cmb, ctb))), 0)
+                        
+                        # The last pixel column for the current row.
+                        cq_end = min(int(np.floor(0.5 + max(cmb, ctb))), width)
+
+                        for cq in range(cq_start, cq_end):
+                            l1, l2, l3 = get_barycentric_coordinates(v1, v2, v3, float(cq) + 0.5, float(rq) + 0.5)
+                            if l1 < 0 or l2 < 0 or l3 < 0:
+                                continue
+
+                            z = v1[2] * l1 + v2[2] * l2 + v3[2] * l3
+
+                            depth_value = depth[cq][rq]
+                            if z < depth_value:
+                                # Current is behind another, don't draw
+                                continue
+
+                            # The uv values of the point
+                            u = v1t[0] * l1 + v2t[0] * l2 + v3t[0] * l3
+                            v = v1t[1] * l1 + v2t[1] * l2 + v3t[1] * l3
+
+                            # Render pixel
+                            s = u * float(cols)
+                            t = (1.0 - v) * float(rows)
+
+                            dl, left_f = np.modf(s)
+                            dr = 1.0 - dl
+                            dt, top_f = np.modf(t)
+                            db = 1.0 - dt
+
+                            left = int(left_f)
+                            top = int(top_f)
+
+                            for i in range(0, num_channels):
+                                value = 0.0
+
+                                tl = float(texture[top][left][i])
+                                tr = float(texture[top][left+1][i])
+                                bl = float(texture[top+1][left][i])
+                                br = float(texture[top+1][left+1][i])
+
+                                value += tl * dr * db
+                                value += tr * dl * db
+                                value += bl * dr * dt
+                                value += br * dl * dt
+
+                                bands[current_band_index + i][cq][rq] = value
+
+                            # Increment the alpha band if the pixel was visible for this band
+                            # the final alpha band will be set to 255 if alpha == num bands
+                            # (all bands have information at this pixel)
+                            alpha_band[cq][rq] += num_channels
+
+                            # Update depth buffer
+                            depth[cq][rq] = z
+            _info("Material %s rendered" % mat)
+
+        current_band_index += num_channels
         primary = False
+
+    # TODO: write TIFF
+    profile = {
+        'driver': 'GTiff',
+        'width': width,
+        'height': height,
+        'count': len(bands),
+        'dtype': data_type,
+        # 'transform': rasterio.transform.Affine(dem_transform[0], dem_transform[1], offset_x, 
+        #                                         dem_transform[3], dem_transform[4], offset_y),
+        # 'nodata': None,
+        # 'crs': crs
+    }
+
+    with rasterio.open(output, 'w', BIGTIFF="IF_SAFER", **profile) as wout:
+        for b in range(len(bands)):
+            wout.write(bands[b], b + 1)
+        #wout.write(alpha_band, num_bands + 1)
 
 
 set_renderer_logger(print, print)
 
-render_orthophoto(["/datasets/brighton2/odm_texturing_25d/odm_textured_model_geo.obj"], 5.0)
+render_orthophoto(["/datasets/brighton2/odm_texturing_25d/odm_textured_model_geo.obj"], 5.0, "/datasets/brighton2/odm_texturing_25d/test.tif")
 
 
 # TODO - Compute area boundaries
