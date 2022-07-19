539d6df0bb1ff52d596320e3184d5aa89aabcb13
Piero Toffanin <pt@masseranolabs.com>
2022-07-19 16:23:03 +0000

    Some numpy acceleration

--- opendm/renderer.py
+++ opendm/renderer.py
@@ -1,5 +1,5 @@
 import rasterio
-from rasterio.plot import reshape_as_image
+# from rasterio.plot import reshape_as_image
 import numpy as np
 import warnings
 import os
@@ -25,7 +25,7 @@ class Bbox:
         self.ymax = -np.inf
     
     def __str__(self):
-        return "[(%s, %s), (%s, %s)]" % (self.xmin, self.ymin, self.xmax, self.ymax)
+        return "%s" % (self.as_array())
     
     def __eq__(self, other):
         if isinstance(other, Bbox):
@@ -43,6 +43,9 @@ class Bbox:
     
     def height(self):
         return self.ymax - self.ymin
+
+    def as_array(self):
+        return np.array([self.xmin, self.ymin, self.xmax, self.ymax])
     
 def set_renderer_logger(info, warn):
     global _info, _warn
@@ -64,12 +67,14 @@ def load_obj(obj_path):
     obj_base_path = os.path.dirname(os.path.abspath(obj_path))
     obj = {
         'materials': {},
+        # 'materials_idx': {},
+        'faces': []
     }
     vertices = []
     uvs = []
 
-    faces = {}
-    current_material = "_"
+    faces = []
+    current_material = None
 
     with open(obj_path) as f:
         _info("Loading %s" % obj_path)
@@ -89,22 +94,28 @@ def load_obj(obj_path):
                 mtl_name = "".join(line.split()[1:]).strip()
                 if not mtl_name in obj['materials']:
                     raise Exception("%s material is missing" % mtl_name)
+                if not 'materials_idx' in obj:
+                    obj['materials_idx'] = list(obj['materials'].keys())
 
-                current_material = mtl_name
+                current_material = obj['materials_idx'].index(mtl_name)
             elif line.startswith("f "):
                 a,b,c = line.split()[1:]
                 av, at = map(int, a.split("/")[0:2])
                 bv, bt = map(int, b.split("/")[0:2])
                 cv, ct = map(int, c.split("/")[0:2])
                 
-                if current_material not in faces:
-                    faces[current_material] = []
+                # if current_material not in faces:
+                #     faces[current_material] = []
 
-                faces[current_material].append(((av - 1, bv - 1, cv - 1), (at - 1, bt - 1, ct - 1))) 
+                #faces[current_material].append(((av - 1, bv - 1, cv - 1), (at - 1, bt - 1, ct - 1))) 
+                #faces.append((current_material, *vertices[av -1], *vertices[bv - 1], *vertices[cv - 1], *uvs[at - 1], *uvs[bt - 1], *uvs[ct - 1]))
+                obj['faces'].append((current_material, av - 1, bv - 1, cv - 1, at - 1, bt - 1, ct - 1))
 
     obj['vertices'] = np.array(vertices)
     obj['uvs'] = np.array(uvs)
-    obj['faces'] = faces
+
+    # for f in faces:
+    #     obj['faces'][f] = np.array(faces[f])
 
     return obj
 
@@ -131,16 +142,15 @@ def load_mtl(mtl_file, obj_base_path):
 
                 # mats[current_mtl] = True
                 with rasterio.open(map_kd, 'r') as r:
-                    mats[current_mtl] = reshape_as_image(r.read())
+                    mats[current_mtl] = r.read() #reshape_as_image(r.read())
     return mats
 
 def compute_bounds(obj):
     bbox = Bbox()
-    for v in obj['vertices']:
-        bbox.xmin = min(bbox.xmin, v[0])
-        bbox.xmax = max(bbox.xmax, v[0])
-        bbox.ymin = min(bbox.ymin, v[1])
-        bbox.ymax = max(bbox.ymax, v[1])
+    bbox.xmin = np.min(obj['vertices'][:,0])
+    bbox.xmax = np.max(obj['vertices'][:,0])
+    bbox.ymin = np.min(obj['vertices'][:,1])
+    bbox.ymax = np.max(obj['vertices'][:,1])
     return bbox
 
 def max_range(dtype):
@@ -149,10 +159,18 @@ def max_range(dtype):
     except ValueError:
         return np.finfo(dtype).max
 
-def is_sliver_polygon(v1, v2, v3):
-    dummy_vec = np.cross(v1 - v2, v3 - v2)
-    return eps2 >= dummy_vec.dot(dummy_vec) / 2.0
+# def is_sliver_polygon(v1, v2, v3):
+#     dummy_vec = np.cross(v1 - v2, v3 - v2)
+#     return eps2 >= dummy_vec.dot(dummy_vec) / 2.0
 
+def filter_sliver_polygons(faces):
+    v1 = faces[:,1:4]
+    v2 = faces[:,4:7]
+    v3 = faces[:,7:10]
+    dummy_vec = np.cross(v1 - v2, v3 - v2)
+    return faces[np.sum(dummy_vec*dummy_vec, axis=1) / 2.0 > eps2]
+
+# TODO: SLOW
 def get_barycentric_coordinates(v1, v2, v3, x, y):
     # Diff along y
     y2y3 = v2[1] - v3[1]
@@ -168,7 +186,7 @@ def get_barycentric_coordinates(v1, v2, v3, x, y):
     norm = y2y3 * x1x3 + x3x2 * y1y3
     l1 = (y2y3 * xx3 + x3x2 * yy3) / norm
     l2 = (y3y1 * xx3 + x1x3 * yy3) / norm
-    l3 = 1 - l1 - l2
+    l3 = 1.0 - l1 - l2
     return (l1, l2, l3)
 
 def render_orthophoto(input_objs, resolution, output):
@@ -224,16 +242,35 @@ def render_orthophoto(input_objs, resolution, output):
         current_band_index = 0
         texture = None
 
-        bands = []
+        all_bands = []
         alpha_band = None
         depth = np.full((width, height), -np.inf, dtype=np.float32)
-        materials_idx = list(obj['materials'].keys())
+        # materials_idx = list(obj['materials'].keys())
 
-        for mat in materials_idx:
+        faces = []
+        vertices = obj['vertices']
+        uvs = obj['uvs']
+
+        # Create faces numpy struct
+        for f in obj['faces']:
+            faces.append((f[0], *vertices[f[1]],*vertices[f[2]],*vertices[f[3]],
+                                *uvs[f[4]], *uvs[f[5]], *uvs[f[6]]))
+        faces = np.array(faces)
+
+        # Remove sliver polygons (if any)
+        num_faces_before = len(faces)
+        faces = filter_sliver_polygons(faces)
+        num_faces = len(faces)
+
+        if num_faces < num_faces_before:
+            _warn("Removed %s sliver polygons" % (num_faces_before - num_faces))
+        
+        mat_idx = 0
+        for mat in obj['materials_idx']:
             texture = obj['materials'][mat]
 
             # First material determines the data type
-            if mat == materials_idx[0]:
+            if mat == obj['materials_idx'][0]:
                 # Init orthophoto
 
                 if primary:
@@ -243,54 +280,40 @@ def render_orthophoto(input_objs, resolution, output):
                     # TODO!
                     pass
                 
-                _info("Texture bands: %s" % texture.shape[2])
+                _info("Texture bands: %s" % texture.shape[0])
                 _info("Texture type: %s" % texture.dtype)
 
-                fill_value = max_range(data_type)
-                for n in range(texture.shape[2]):
-                    band = np.full((width, height), fill_value, dtype=data_type)
-                    bands.append(band)
+                bands = np.full((texture.shape[0], width, height), max_range(data_type), dtype=data_type)
                 alpha_band = np.zeros((width, height), dtype=data_type)
-        
+                all_bands.append(bands)
 
             # Render
 
-            rows, cols = map(float, texture.shape[:2])
-            num_channels = texture.shape[2]
+            rows, cols = map(float, texture.shape[1:])
+            num_channels = texture.shape[0]
 
             # For each face in the current material
-            for f in obj['faces'][mat]:
+            mat_faces = faces[faces[:,0] == mat_idx]
+
+            for f in mat_faces:
                 # Draw textured triangle
 
-                v1i, v2i, v3i = f[0]
-                v1 = obj['vertices'][v1i]
-                v2 = obj['vertices'][v2i]
-                v3 = obj['vertices'][v3i]
+                # v1i, v2i, v3i = f[0]
+                # v1 = obj['vertices'][v1i]
+                # v2 = obj['vertices'][v2i]
+                # v3 = obj['vertices'][v3i]
 
-                if is_sliver_polygon(v1, v2, v3):
-                    _warn("Sliver polygon found at %s" % f)
-                    continue
+                v = f[1:10]
+                t = f[10:]
 
-                u1i, u2i, u3i = f[1]
-                v1t = obj['uvs'][u1i]
-                v2t = obj['uvs'][u2i]
-                v3t = obj['uvs'][u3i]
+                v1 = v[0:3]
+                v2 = v[3:6]
+                v3 = v[6:9]
 
-                # Check bounding box overlap
-                
-                minx = int(min((v1[0], v2[0], v3[0])))
-                if minx > width:
-                    continue # Completely outside to the right
-                maxx = int(max((v1[0], v2[0], v3[0])))
-                if maxx < 0:
-                    continue # Completely outside to the left
-                miny = int(min((v1[1], v2[1], v3[1])))
-                if miny > height:
-                    continue # Completely outside to the top.
-                maxy = int(max((v1[1], v2[1], v3[1])))
-                if maxy < 0:
-                    continue # Completely outside to the bottom.
-                
+                v1t = t[0:2]
+                v2t = t[2:4]
+                v3t = t[4:6]
+
                 # Top point row and column positions
                 top_r = top_c = None
                 # Middle point row and column positions
@@ -386,7 +409,7 @@ def render_orthophoto(input_objs, resolution, output):
 
                             z = v1[2] * l1 + v2[2] * l2 + v3[2] * l3
 
-                            depth_value = depth[cq][rq]
+                            depth_value = depth[cq,rq]
                             if z < depth_value:
                                 # Current is behind another, don't draw
                                 continue
@@ -394,7 +417,7 @@ def render_orthophoto(input_objs, resolution, output):
                             # The uv values of the point
                             u = v1t[0] * l1 + v2t[0] * l2 + v3t[0] * l3
                             v = v1t[1] * l1 + v2t[1] * l2 + v3t[1] * l3
-
+                            
                             # Render pixel
                             s = u * float(cols)
                             t = (1.0 - v) * float(rows)
@@ -410,25 +433,25 @@ def render_orthophoto(input_objs, resolution, output):
                             for i in range(0, num_channels):
                                 value = 0.0
 
-                                tl = float(texture[top][left][i])
-                                tr = float(texture[top][left+1][i])
-                                bl = float(texture[top+1][left][i])
-                                br = float(texture[top+1][left+1][i])
+                                tl = float(texture[i,top,left])
+                                tr = float(texture[i,top,left+1])
+                                bl = float(texture[i,top+1,left])
+                                br = float(texture[i,top+1,left+1])
 
                                 value += tl * dr * db
                                 value += tr * dl * db
                                 value += bl * dr * dt
                                 value += br * dl * dt
 
-                                bands[current_band_index + i][cq][rq] = value
+                                bands[current_band_index + i,cq,rq] = value
 
                             # Increment the alpha band if the pixel was visible for this band
                             # the final alpha band will be set to 255 if alpha == num bands
                             # (all bands have information at this pixel)
-                            alpha_band[cq][rq] += num_channels
+                            alpha_band[cq,rq] += num_channels
 
                             # Update depth buffer
-                            depth[cq][rq] = z
+                            depth[cq,rq] = z
                 
                 if eps < bot_r - mid_r:
                     cmbdr = (bot_c - mid_c) / (bot_r - mid_r)
@@ -460,7 +483,7 @@ def render_orthophoto(input_objs, resolution, output):
 
                             z = v1[2] * l1 + v2[2] * l2 + v3[2] * l3
 
-                            depth_value = depth[cq][rq]
+                            depth_value = depth[cq,rq]
                             if z < depth_value:
                                 # Current is behind another, don't draw
                                 continue
@@ -484,26 +507,28 @@ def render_orthophoto(input_objs, resolution, output):
                             for i in range(0, num_channels):
                                 value = 0.0
 
-                                tl = float(texture[top][left][i])
-                                tr = float(texture[top][left+1][i])
-                                bl = float(texture[top+1][left][i])
-                                br = float(texture[top+1][left+1][i])
+                                tl = float(texture[i,top,left])
+                                tr = float(texture[i,top,left+1])
+                                bl = float(texture[i,top+1,left])
+                                br = float(texture[i,top+1,left+1])
 
                                 value += tl * dr * db
                                 value += tr * dl * db
                                 value += bl * dr * dt
                                 value += br * dl * dt
 
-                                bands[current_band_index + i][cq][rq] = value
+                                bands[current_band_index + i,cq,rq] = value
 
                             # Increment the alpha band if the pixel was visible for this band
                             # the final alpha band will be set to 255 if alpha == num bands
                             # (all bands have information at this pixel)
-                            alpha_band[cq][rq] += num_channels
+                            alpha_band[cq,rq] += num_channels
 
                             # Update depth buffer
-                            depth[cq][rq] = z
+                            depth[cq,rq] = z
             _info("Material %s rendered" % mat)
+
+            mat_idx += 1
 
         current_band_index += num_channels
         primary = False
@@ -522,8 +547,8 @@ def render_orthophoto(input_objs, resolution, output):
     }
 
     with rasterio.open(output, 'w', BIGTIFF="IF_SAFER", **profile) as wout:
-        for b in range(len(bands)):
-            wout.write(bands[b], b + 1)
+        #for b in range(len(bands)):
+        wout.write(bands)
         #wout.write(alpha_band, num_bands + 1)
 
 
