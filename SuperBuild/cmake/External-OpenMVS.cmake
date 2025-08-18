e2ac8eaee50cb5953f5832c40f9a207a00e6bacc
Piero Toffanin <pt@masseranolabs.com>
2025-08-18 15:34:36 -0400

    Update README, repo refs

--- SuperBuild/cmake/External-OpenMVS.cmake
+++ SuperBuild/cmake/External-OpenMVS.cmake
@@ -53,7 +53,7 @@ ExternalProject_Add(${_proj_name}
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
   GIT_REPOSITORY    https://github.com/pierotofy/openMVS
-  GIT_TAG           400
+  GIT_TAG           390
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
