51077915647bfb5ca8d2d2b09b67bb0e4361383c
Piero Toffanin <pt@masseranolabs.com>
2025-08-25 11:24:54 -0400

    Update mvs-texturing

--- SuperBuild/cmake/External-MvsTexturing.cmake
+++ SuperBuild/cmake/External-MvsTexturing.cmake
@@ -8,8 +8,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/mvs-texturing
-  GIT_TAG           290
+  GIT_REPOSITORY    https://github.com/pierotofy/mvs-texturing
+  GIT_TAG           390
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
