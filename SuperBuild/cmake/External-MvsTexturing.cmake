34f5c98f09db7d57b2de319588258280fa2f8fb3
Piero Toffanin <pt@masseranolabs.com>
2025-08-26 01:25:20 -0400

    Build updates

--- SuperBuild/cmake/External-MvsTexturing.cmake
+++ SuperBuild/cmake/External-MvsTexturing.cmake
@@ -8,8 +8,8 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}/${_proj_name}
-  GIT_REPOSITORY    https://github.com/pierotofy/mvs-texturing
-  GIT_TAG           390
+  GIT_REPOSITORY    ssh://git@github.com/pierotofy/mvs-texturing.git
+  GIT_TAG           391
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
