737780fa208b075fd9343dd7aeb1b8158574a30e
Piero Toffanin <pt@masseranolabs.com>
2025-08-27 08:02:27 -0400

    Bump OpenSfM

--- SuperBuild/cmake/External-FPCFilter.cmake
+++ SuperBuild/cmake/External-FPCFilter.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/FPCFilter
+  GIT_REPOSITORY    https://github.com/pierotofy/FPCFilter
   GIT_TAG           331
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -25,7 +25,7 @@ ExternalProject_Add(${_proj_name}
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
   GIT_REPOSITORY    https://github.com/pierotofy/OpenSfM/
-  GIT_TAG           390
+  GIT_TAG           392
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
   #--Configure step-------------
