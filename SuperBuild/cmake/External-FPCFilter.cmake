2214530af491bd649ef48d3886a1b8300bea9a28
Piero Toffanin <pt@masseranolabs.com>
2025-09-01 15:51:51 -0400

    Bump fpcfilter

--- SuperBuild/cmake/External-FPCFilter.cmake
+++ SuperBuild/cmake/External-FPCFilter.cmake
@@ -8,7 +8,7 @@ ExternalProject_Add(${_proj_name}
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
   GIT_REPOSITORY    https://github.com/pierotofy/FPCFilter
-  GIT_TAG           331
+  GIT_TAG           396
   #--Update/Patch step----------
   UPDATE_COMMAND    ""
   #--Configure step-------------
